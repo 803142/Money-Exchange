88486527b8cdcae6ebf3440db74052cd5700d771
803142 <803142@gmail.com>
2019-03-13 11:30:33 +0300

    fix: solved throw for..in

--- src/index.js
+++ src/index.js
@@ -1,36 +1,20 @@
 module.exports = function makeExchange(currency) {
-const change = {};
-if (currency>=10000){
-change={error: "You are rich, my friend! We don't have so much coins for exchange"}; 
-}
-else{
+  const change = {};
+  const base = {"H":50,"Q":25,"D":10,"N":5,"P":1};
 
-few=currency%50;
-if (currency/50>=1){
-    change[['H']]=((currency-few)/50);
-    currency=few;
-}
-few=currency%25;
-if (currency/25>=1){
-    change['Q']=((currency-few)/25);
-    currency=few;
-}
-few=currency%10;
-if (currency/10>=1){
-    change['D']=((currency-few)/10);
+  let few = 0;
+  let exchange = currency;
 
-    currency=few;
-}
-few=currency%5;
-if (currency/5>=1){
-    change['N']=((currency-few)/5);
-
-    currency=few;
-}
-if (currency>=1){
-    change['P']=few;
-}
-
-}
-return change;
+  if (exchange >= 10000){
+    change.error = "You are rich, my friend! We don't have so much coins for exchange"; 
+  }else {
+    for(let key in base) {
+      few = exchange%base[key];
+      if (exchange/base[key] >= 1){
+        change[key] = ((exchange-few)/base[key]);
+        exchange = few;
+       }
+    }
+  }
+  return change;
 }
