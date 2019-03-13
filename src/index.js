baa0f3d11a2f7f727f1b58fa1f628e8f64e36410
803142 <803142@gmail.com>
2019-03-13 09:54:23 +0300

    style: some style changes

--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-    var change = {};
-    if (currency>=10000){
-       change={error: "You are rich, my friend! We don't have so much coins for exchange"}; 
-    }
-    else{
-        
-        few=currency%50;
-        if (currency/50>=1){
-            change[['H']]=((currency-few)/50);
-            currency=few;
-        }
-        few=currency%25;
-        if (currency/25>=1){
-            change['Q']=((currency-few)/25);
-            currency=few;
-        }
-        few=currency%10;
-        if (currency/10>=1){
-            change['D']=((currency-few)/10);
+const change = {};
+if (currency>=10000){
+change={error: "You are rich, my friend! We don't have so much coins for exchange"}; 
+}
+else{
 
-            currency=few;
-        }
-        few=currency%5;
-        if (currency/5>=1){
-            change['N']=((currency-few)/5);
+few=currency%50;
+if (currency/50>=1){
+    change[['H']]=((currency-few)/50);
+    currency=few;
+}
+few=currency%25;
+if (currency/25>=1){
+    change['Q']=((currency-few)/25);
+    currency=few;
+}
+few=currency%10;
+if (currency/10>=1){
+    change['D']=((currency-few)/10);
 
-            currency=few;
-        }
-        if (currency>=1){
-            change['P']=few;
-        }
+    currency=few;
+}
+few=currency%5;
+if (currency/5>=1){
+    change['N']=((currency-few)/5);
 
+    currency=few;
+}
+if (currency>=1){
+    change['P']=few;
+}
 
-    }
-     return change;
+}
+return change;
 }
